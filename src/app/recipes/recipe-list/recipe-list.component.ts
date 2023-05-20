ef665e0d64f7e51e100fee0ab73c19a0458f9f2a
Andrés Camilo Piratova Pineda <azprodiyi@gmail.com>
2023-05-19 23:05:47 -0500

    Passing Data

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model'
 
 @Component({
@@ -6,7 +6,8 @@ import { Recipe } from '../recipe.model'
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit{
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   recipes: Recipe[] = [
     new Recipe('capuchino','bebida espumosa','https://www.recetasderechupete.com/wp-content/uploads/2021/03/Capuchino-perfecto.jpg'),
     new Recipe('helado','postre frio','https://ken-foods.com/en/wp-content/uploads/2018/07/el-origen-del-helado-e1530726550260.jpg'),
@@ -16,5 +17,9 @@ export class RecipeListComponent implements OnInit{
 
   ngOnInit() {
 
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeWasSelected.emit(recipe);
   }
 }
